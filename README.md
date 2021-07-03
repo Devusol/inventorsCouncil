7ae6353770134b5d333db79b395e9fdb2de75d72
Devusol <dev.dusol@gmail.com>
2021-07-03 16:09:33 -0400

    breaking up sections for javascript automation

--- README.md
+++
@@ -1,2 +0,0 @@
-# inventorsCouncil
-Neil Project
---
+++ assets/js/partials.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("head").load("/html/shared/head.html");
+  $("header").load("/html/shared/navbar.html");
+  $("footer").load("/html/shared/footer.html");
+});
+
+
