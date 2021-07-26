893ec52d7f809da3d11307a4500188bce4c95c0f
Giselle Borrero <giselle.a.borrero@gmail.com>
2021-07-26 12:09:14 -0400

    testimonial slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,19 @@
 // Contact Page - Contact Form JS
 let theForm = document.getElementById("contactPageForm");
-theForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log(this);
-  let input = this.elements;
-  var contactForm = new FormData(theForm);
-  console.log([...contactForm]);
-/*   let formPairs = [];
-  for (let pair of contactForm.entries()) {
-    formPairs.push(pair);
-  } 
-  console.log(formPairs); */
+theForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    console.log(this);
+    let input = this.elements;
+    var contactForm = new FormData(theForm);
+    console.log([...contactForm]);
+    /*   let formPairs = [];
+      for (let pair of contactForm.entries()) {
+        formPairs.push(pair);
+      } 
+      console.log(formPairs); */
 });
+
+
+$('.carousel').carousel({
+    interval: 2000
+})
