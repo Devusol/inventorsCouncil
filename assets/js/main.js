edf97328936362470d0c82073368dabf65f88a54
Devusol <info@devusol.com>
2021-08-05 15:24:28 -0400

    tweaking nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,52 @@
 // Contact Page - Contact Form JS
 let theForm = document.getElementById("contactPageForm");
-theForm.addEventListener("submit", function(e) {
-    e.preventDefault();
-    console.log(this);
-    let input = this.elements;
-    var contactForm = new FormData(theForm);
-    console.log([...contactForm]);
-    /*   let formPairs = [];
-      for (let pair of contactForm.entries()) {
-        formPairs.push(pair);
-      } 
-      console.log(formPairs); */
+theForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(this);
+  let input = this.elements;
+  var contactForm = new FormData(theForm);
+  console.log([...contactForm]);
+  /*   let formPairs = [];
+    for (let pair of contactForm.entries()) {
+      formPairs.push(pair);
+    } 
+    console.log(formPairs); */
 });
 
 
 $('.carousel').carousel({
-    interval: 2000
+  interval: 2000
 })
 
+function navPop(params) {
+
+  $('.nav-mobile').each(function () {
+
+    if (params.innerText == this.innerText) {
+      /*  $.get('/html/about.html', null, htmlResponse =>{ */
+      $.get(`/html/${params.innerText.split(" ")[0].toLowerCase()}.html`, null, htmlResponse => {
+        let navItems = $(htmlResponse).find('.sub-navbar-content');
+        let linkTo;
+        for (let item of $('#navbarSupportedContent').find(`a`)) {
+          if (item.innerText == params.innerText) {
+            console.log(item.href);
+            linkTo = item;
+          }
+        }
+        $('.mobile-navbar-items').empty();
+        $('.mobile-navbar-items').append(linkTo, navItems);
+      });
+    }
+    // console.log(this.innerText);
+  })
+
+  /*   if (params.name == "about") {
+      $.get('/html/about.html', null, htmlResponse => {
+         let navItems = $('.sub-navbar-menu');
+        $('.mobile-navbar-items').append(navItems); 
+        console.log(navItems, $('.mobile-navbar-items')); 
+        console.log('pop pop');
+      });
+    } 
+   */
+}
