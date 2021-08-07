f6a25175896fb59ab4f6cc5eb25468632709f6ff
Devusol <info@devusol.com>
2021-08-07 12:10:36 -0400

    updated with final contact mailer api url

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,28 @@
 // Contact Page - Contact Form JS
-let theForm = document.getElementById("contactPageForm");
+let contactForm = document.querySelector(".contact-form");
 let whichPage = window.location.pathname;
 
-//Set logo whit on index page and colorful on all other pages
-if (whichPage == "/" | whichPage == "/index.html"){
-  $("#brand-logo").attr("src","/assets/img/logowhite.png");
+//Set logo white on index page and colorful on all other pages
+if (whichPage == "/" | whichPage == "/index.html") {
+  $("#brand-logo").attr("src", "/assets/img/logowhite.png");
 }
 
-
-
-theForm.addEventListener("submit", function (e) {
+contactForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(this);
-  let input = this.elements;
-  var contactForm = new FormData(theForm);
-  console.log([...contactForm]);
-  /*   let formPairs = [];
-    for (let pair of contactForm.entries()) {
-      formPairs.push(pair);
-    } 
-    console.log(formPairs); */
+  const contactForm = new FormData(e.target);
+  const formValues = Object.fromEntries(contactForm.entries());
+
+  fetch('https://app.devusol.com/aproxy/iccf/api/v1/nodemail', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(formValues)
+  }).then(response => {
+    e.target.reset();
+    alert('Thank you. Your request has been sent.');
+    console.log(response.json());
+  })
 });
 
 
